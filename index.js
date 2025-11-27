f6d9a861c3675550739736684383350b2339d2ea
abch20 <abdellahchellouch@gmail.com>
2025-11-27 12:41:33 +0100

    the file is added successfuly

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
+import logger from "./logger.js";
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          methode: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
 
 // add new tea
 app.post("/teas", (req, res) => {
+  logger.warn("Received request to add new tea");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
---
+++ logger.js
@@ -0,0 +1,23 @@
+import winston from "winston";
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, json, colorize } = format;
+
+const consoleLogFormat = format.combine(
+  format.colorize(),
+  format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+  })
+);
+
+const logger = createLogger({
+  level: "info",
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+    new transports.File({ filename: "logs/app.log" }),
+  ],
+});
+
+export default logger;
